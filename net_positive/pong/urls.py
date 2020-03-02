adef705a501143d2b75f0f9f951e29547ff63eff
David Papamichael <davidpapamichael@gmail.com>
2020-03-02 16:04:15 +0000

    feature: 2nd path added, structure set up

--- net_positive/pong/urls.py
+++ net_positive/pong/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='pong-home'),
+    path('play/', views.play, name='pong-play'),
 ]
--- net_positive/pong/views.py
+++ net_positive/pong/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1> Pong Home </h1>')
+
+def play(request):
+    return HttpResponse('<h1> Pong Play </h3>')

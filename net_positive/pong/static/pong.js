f531154505734a95c0d1e95ee77cf00b3a82ccc0
Asia <asiaellis5@gmail.com>
2020-03-03 13:10:21 +0000

    feat: ball moving added and runs through django

--- net_positive/pong/static/pong.js
+++ net_positive/pong/static/pong.js
@@ -1,10 +1,92 @@
-// creates the canvas
+'use strict';
+
+class Vector
+{
+  constructor(x = 0, y = 0)
+  {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Rectangle
+{
+  constructor(w, h)
+  {
+    this.position = new Vector;
+    this.size = new Vector(w, h)
+  }
+  get left()
+  {
+    return this.position.x - this.size.x / 2;
+  }
+  get right()
+  {
+    return this.position.x + this.size.x / 2;
+  }
+  get top()
+  {
+    return this.position.y - this.size.y / 2;
+  }
+  get bottom()
+  {
+    return this.position.y + this.size.y / 2;
+  }
+}
+
+class Ball extends Rectangle
+{
+  constructor()
+  {
+    super(10, 10)
+    this.velocity = new Vector;
+  }
+}
+
+class Pong
+{
+  constructor(canvas)
+  {
+    this._canvas = canvas;
+    this._context = canvas.getContext('2d');
+
+    this.ball = new Ball;
+    this.ball.position.x = 100;
+    this.ball.position.y = 50;
+
+    this.ball.velocity.x = 100;
+    this.ball.velocity.y = 100;
+
+   let lastTime;
+   const callback = (milliseconds) => {
+      if (lastTime) {
+        this.update((milliseconds - lastTime) / 1000);
+      }
+      lastTime = milliseconds;
+      requestAnimationFrame(callback);
+    }
+    callback();
+  }
+  update(deltatime) {
+    this.ball.position.x += this.ball.velocity.x * deltatime;
+    this.ball.position.y += this.ball.velocity.y * deltatime;
+  
+    if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+      this.ball.velocity.x = -this.ball.velocity.x
+    }
+  
+    if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+      this.ball.velocity.y = -this.ball.velocity.y
+    }
+  
+    this._context.fillStyle = '#000';
+    this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+  
+    this._context.fillStyle = '#fff';
+    this._context.fillRect(this.ball.position.x, this.ball.position.y, this.ball.size.x, this.ball.size.y);
+  
+  }
+}
+
 const canvas = document.getElementById('pong');
-// makes the object 2d and includes methods and properties for drawing
-const context = canvas.getContext('2d');
-// fills the canvas with black (specified width and height in html)
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
-// creates a white ball 10 x 10 pixles
-context.fillStyle = '#fff';
-context.fillRect(0, 0, 10, 10);
+const pong = new Pong(canvas);

1ef72c7ee647cb9aa7e0aa31caca14c041c3c83d
ratcliffetj <ratcliffetj89@gmail.com>
2020-03-03 12:12:21 +0000

    serve pong.js from index page

--- net_positive/pong/views.py
+++ net_positive/pong/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse(
-'<!DOCTYPE html><html lang="en"><head><title>Pong</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><h1>Pong</h1><script src="pong.js"></script></body>')
+'<!DOCTYPE html><html lang="en"><head><title>Pong</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><h1>Pong</h1><canvas id="pong" width="600" height="400"></canvas><script src="static/pong.js"></script></body>')
 
 def play(request):
     return HttpResponse('<h1> Pong Play </h3>')

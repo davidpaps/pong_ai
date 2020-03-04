5041089db40051959c47eb5fd4597f1ba61757d8
Asia <asiaellis5@gmail.com>
2020-03-04 14:33:48 +0000

    test: unit tests for size of the players and ball

--- net_positive/spec/javascripts/support/pongSpec.js
+++ net_positive/spec/javascripts/support/pongSpec.js
@@ -4,6 +4,9 @@ describe('Pong', function() {
   var pong;
   var testCanvas;
   var fakeDocument;
+  var ball;
+  var rectangle;
+  var player;           
 
 
   beforeEach(function() {
@@ -11,11 +14,13 @@ describe('Pong', function() {
       height: 150,
       width: 150,
       getContext: function() {
-        return ""
+        
       }
     }
   
     pong = new Pong(testCanvas);
+    rectangle = new Rectangle()
+    player = new Player()
   })
 
   describe("canvas dimensions", function() {
@@ -24,6 +29,22 @@ describe('Pong', function() {
     })
     it("draws the correct width", function() {
       expect(pong._canvas.width).toEqual(150)
+    })
+  })
+
+  describe("ball", function() {
+    it("renders in the correct size", function() {
+      ball = new Ball()
+      expect(ball.size.x).toEqual(10)
+      expect(ball.size.y).toEqual(10)
+    })
+  })
+
+  describe("player", function() {
+    it("it renders in the correct size", function() {
+      player = new Player()
+      expect(player.size.x).toEqual(20)
+      expect(player.size.y).toEqual(100)
     })
   })
 

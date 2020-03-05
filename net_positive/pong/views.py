b8131b493493e8dc5cae823142d76fe289486d17
Jakephillips55 <Jakep629@gmail.com>
2020-03-05 12:12:22 +0000

    Now have working Template structure for loading html / js in django

--- net_positive/pong/views.py
+++ net_positive/pong/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from pong.models import SimpleBot
 
-def home(request):
-    return HttpResponse(
-'<!DOCTYPE html><html lang="en"><head><title>Pong</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><h1>Pong</h1><canvas id="pong" width="600" height="400"></canvas><script src="static/pong.js"></script></body>')
+def home(request, template='index.html'):
+    return render(request, template, {})
 
 def bot(request):
 

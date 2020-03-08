3401a9fd917c20e8b002a457dc80059bc0c83dae
ratcliffetj <ratcliffetj89@gmail.com>
2020-03-08 15:59:16 +0000

    Passed simple bot response into javascript

--- net_positive/pong/views.py
+++ net_positive/pong/views.py
@@ -21,10 +21,7 @@ def play(request):
     return HttpResponse('<h1> Pong Play </h3>')
 
 def wsbot(request, training_session):
-  print(message.content)
+ 
   return render(request, 'pong/wsbot.html', {
-        # 'message2' : message.content['message'],
         'training_session': training_session,
-        'up': SimpleBot.simple_bot({'bally': '150', 'paddley': '120', 'reward': '0'}),
-
     })
